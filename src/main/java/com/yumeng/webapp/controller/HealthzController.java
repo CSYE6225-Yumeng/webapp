72328d9f0a10ccc9d18d8c6661c5cc2fd0e996cf
Yumeng Huang <lydiavitani@gmail.com>
2023-01-29 20:49:52 -0800

    healthz

--- src/main/java/com/yumeng/webapp/controller/HealthzController.java
+++ src/main/java/com/yumeng/webapp/controller/HealthzController.java
@@ -1,17 +1,15 @@
 package com.yumeng.webapp.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class UserController {
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+public class HealthzController {
+    @GetMapping("/healthz")
+    public ResponseEntity getHealthz() {
+        return ResponseEntity.ok().build();
     }
-
-
 }
