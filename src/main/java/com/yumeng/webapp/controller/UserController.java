cf73491ca99f8f613bb0087e4fec779d0d4871f8
Yumeng Huang <lydiavitani@gmail.com>
2023-01-29 20:35:33 -0800

    init

---
+++ src/main/java/com/yumeng/webapp/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.yumeng.webapp.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class UserController {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
