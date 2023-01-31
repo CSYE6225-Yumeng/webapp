aefc8074ab457fcd73f5f3eb1ec27646a301b525
Yumeng Huang <lydiavitani@gmail.com>
2023-01-31 13:55:03 -0800

    api

--- src/main/java/com/yumeng/webapp/config/SecurityConfiguration.java
+++ src/main/java/com/yumeng/webapp/config/SecurityConfiguration.java
@@ -3,23 +3,46 @@ package com.yumeng.webapp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.sql.DataSource;
+import java.io.Serializable;
+import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.cors().disable()
-                .csrf().disable()
-                .authorizeHttpRequests().requestMatchers("/v1/user").permitAll().and()
-                .build();
+          http.csrf().disable()
+                  .authorizeHttpRequests()
+                  .requestMatchers("/v1/user").permitAll()
+                  .requestMatchers("/healthz").permitAll()
+                  .anyRequest().authenticated()
+                  .and().httpBasic();
+
+          return http.build();
     }
+
 }
