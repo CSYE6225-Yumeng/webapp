0aa64b00f4d47ec6e69c334260468eeb60ed396f
Yumeng Huang <lydiavitani@gmail.com>
2023-02-07 22:53:29 -0800

    finish update product

--- src/main/java/com/yumeng/webapp/repository/ProductRepository.java
+++ src/main/java/com/yumeng/webapp/repository/ProductRepository.java
@@ -8,6 +8,7 @@ import com.yumeng.webapp.util.HibernateUtil;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.postgresql.util.PSQLException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -32,5 +33,46 @@ public class ProductRepository {
         session.save(product);
         transaction.commit();
         return product.getProductResponse();
+    }
+
+    public Product hasPermission(String userId, long ProductId) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Product product = session.get(Product.class, ProductId);
+        transaction.commit();
+        if(product != null){
+            if(userId.equals(Long.toString(product.getUser().getId()))){
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product updateProduct(Product product, Product getProduct) throws PSQLException {
+        String name = product.getName();
+        if(name != null){
+            getProduct.setName(name);
+        }
+        String description = product.getDescription();
+        if(description != null){
+            getProduct.setDescription(description);
+        }
+        String sku = product.getSku();
+        if(sku != null){
+            getProduct.setSku(sku);
+        }
+        String manufacturer = product.getManufacturer();
+        if(manufacturer != null){
+            getProduct.setManufacturer(manufacturer);
+        }
+        Integer quantity = product.getQuantity();
+        if(quantity != null){
+            getProduct.setQuantity(quantity);
+        }
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(getProduct);
+        transaction.commit();
+        return getProduct;
     }
 }
