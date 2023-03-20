1520665506fc8618beef13d0b198886fc3a5431f
Yumeng Huang <lydiavitani@gmail.com>
2023-03-20 12:05:13 -0700

    add cloud-watch

--- packer/launch_app.sh
+++ packer/launch_app.sh
@@ -1,6 +1,13 @@
 sudo chmod +x /opt/deployment/webapp-0.0.1-SNAPSHOT.jar
 #sudo java -jar /opt/deployment/webapp-0.0.1-SNAPSHOT.jar
 
+#cloudWatch
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c /opt/deployment/cloudwatch_config.json \
+    -s
+
 #systemd
 sudo chmod -R 777 /etc/init.d
 sudo ln -s /opt/deployment/webapp-0.0.1-SNAPSHOT.jar /etc/init.d/webapp
--- src/main/java/com/yumeng/webapp/config/JdbiConfiguration.java
+++ src/main/java/com/yumeng/webapp/config/JdbiConfiguration.java
@@ -3,6 +3,8 @@ package com.yumeng.webapp.config;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.postgres.PostgresPlugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class JdbiConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(AWSConfiguration.class);
     @Bean
     public Jdbi jdbi(DataSource dataSource){  //@Autowired DataSource dataSource
         return Jdbi.create(dataSource)
