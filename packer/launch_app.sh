89eeb820d6a98f750cc8691d3ee7bf5267889d0c
Yumeng Huang <lydiavitani@gmail.com>
2023-04-12 10:43:15 -0700

    start coding

--- packer/launch_app.sh
+++ packer/launch_app.sh
@@ -19,3 +19,38 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -m ec2 \
     -c file:/opt/deployment/cloudwatch_config.json \
     -s
+
+# AWS CLI - Create a new Launch Template for the autoscaling group
+
+aws configure set aws_access_key_id ${{ secrets.DEMO_AWS_ACCESS_KEY_ID }} --profile demo
+aws configure set aws_secret_access_key ${{ secrets.DEMO_AWS_SECRET_ACCESS_KEY }} --profile demo
+aws configure set region $REGION --profile demo
+aws configure set aws_access_key_id ${{ secrets.DEV_AWS_ACCESS_KEY_ID }} --profile dev
+aws configure set aws_secret_access_key ${{ secrets.DEV_AWS_SECRET_ACCESS_KEY }} --profile dev
+aws configure set region $REGION --profile dev
+
+export LAUNCH_TEMPLATE_ID=`aws ec2 describe-launch-templates  \
+--region $REGION \
+--profile $PROFILE_NAME \
+--query 'LaunchTemplates[0].LaunchTemplateId' \
+--output text`
+
+export IMAGE_ID=`aws ec2 describe-images \
+--region $REGION \
+--profile $PROFILE_NAME \
+--filters "Name=name,Values=yumeng_*" \
+--query 'sort_by(Images, &CreationDate)[-1].ImageId' \
+--output text`
+
+aws ec2 create-launch-template-version \
+  --launch-template-id $LAUNCH_TEMPLATE_ID \
+  --version-description new_version \
+  --source-version 1 \
+  --launch-template-data "{\"ImageId\":\"${IMAGE_ID}\"}" \
+  --region $REGION \
+  --profile $PROFILE_NAME
+
+aws autoscaling start-instance-refresh \
+    --auto-scaling-group-name $AUTO_SCALING_GROUP_NAME \
+    --region $REGION \
+    --profile $PROFILE_NAME
